e9ad5773cd88f4a8f3b435fb37deb3ead40bb03e
Draweshwarkumar <shivamjha22948@gmail.com>
2024-07-26 20:21:00 +0530

    creating unique id

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4: uuidv4} = require('uuid');
+
 
 app.use(express.urlencoded({extended: true}));
 
@@ -12,16 +14,19 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
+        id: uuidv4(),
         username: "shivam jha",
         district: "Darbhanga",
         status: "java developer "
     },
     {
+        id: uuidv4(),
         username: "Draweshwar kumar",
         district: "Patna",
         status: "MERN developer"
     },
     {
+        id: uuidv4(),
         username: "pankaj kumar",
         district: "Chapra",
         status: "Bodybuilder"
@@ -37,9 +42,15 @@ app.get("/posts/new",(req,res) =>{
 
 app.post("/posts",(req, res) =>{
     let {username,district,status} = req.body;
-    posts.push({username,district,status});
-    res.send("post request working")
+    let id = uuidv4();
+    posts.push({id,username,district,status});
+    res.redirect("/posts");
 });
+app.get("/posts/:id",(req,res) =>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs",{post});
+})
 
 app.listen(port, () =>{
     console.log(`app is listening on the port ${port}`);
