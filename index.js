150d0ccbae407be4d2189ae11ce5be7b1b338743
Draweshwarkumar <shivamjha22948@gmail.com>
2024-07-27 19:20:29 +0530

    patch/delte

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const {v4: uuidv4} = require('uuid');
+const methodOverride = require("method-override");
 
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -50,6 +52,24 @@ app.get("/posts/:id",(req,res) =>{
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs",{post});
+});
+app.patch("/posts/:id",(req,res) => {
+    let{id} = req.params;
+    let newstatus = req.body.status;
+    let post = posts.find((p)=> id === p.id);
+    post.status = newstatus;
+    console.log(post);
+    res.redirect("/posts"); 
+});
+app.get("/posts/:id/edit",(req,res) =>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs",{post});
+});
+app.delete("/posts/:id" ,(req,res) =>{
+    let {id} = req.params;
+     posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts"); 
 })
 
 app.listen(port, () =>{
