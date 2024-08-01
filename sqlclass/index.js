838b2911a29829a8eb5723bfc580c23302045552
Draweshwarkumar <shivamjha22948@gmail.com>
2024-08-01 20:41:25 +0530

    insert data in database

--- sqlclass/index.js
+++ sqlclass/index.js
@@ -1,10 +1,32 @@
- const {faker} = require('@faker-js/faker');
+const mysql = require('mysql2');
+const { faker } = require('@faker-js/faker');
 
 const connection = mysql.createConnection({
-   host: "localhost",
-   user: "root",
-   database: "delta_app",
-})
+    host: "localhost",
+    user: "root",
+    database: "delta_app",
+    password: "massey@1035"
+});
+
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+let users = [
+    ["123b", "123_newusere", "abc@gmail.come", "abce"],
+    ["123c", "123_newuserf", "abcb@gmail.comf", "abcdf"],
+    ["123d", "123_newuserg", "abcc@gmail.comg", "abcdg"]
+];
+
+try {
+    connection.query(q, [users], (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+} catch (err) {
+    console.log(err);
+}
+
+connection.end();
+
+
 
  let createRandomuser = () => {
     return{
@@ -17,4 +39,4 @@ const connection = mysql.createConnection({
     registeredAt: faker.date.past()
     };  
  }
- console.log(createRandomuser());
+ 
